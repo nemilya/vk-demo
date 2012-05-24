7801a88206d8df91bdfb26c4c4f9e1fc8714cb37
Ilya Nemihin <nemilya@gmail.com>
2012-05-24 16:58:26 +0600

    added bootstrap

--- app.rb
+++ app.rb
@@ -7,22 +7,7 @@ require 'vkontakte_api'
 
 enable :sessions
 
-config_path = File.join("config", "oauth.yml")
-if File.exists?(config_path)
-  oauth_settings = YAML.load(File.read(config_path))
-
-  if oauth_settings['vkontakte']
-    ENV['API_KEY']    ||= oauth_settings['vkontakte']['api_key']
-    ENV['API_SECRET'] ||= oauth_settings['vkontakte']['api_secret']
-  end
-
-  use OmniAuth::Builder do  
-    if ENV['API_KEY']
-      provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET'],
-        :scope => 'friends,audio,photos', :display => 'popup'
-    end
-  end
-end
+require 'init_vk'
 
 before do
   @app = VkontakteApi::Client.new(session[:token]) if session[:token]
---
+++ init_vk.rb
@@ -0,0 +1,16 @@
+config_path = File.join("config", "oauth.yml")
+if File.exists?(config_path)
+  oauth_settings = YAML.load(File.read(config_path))
+
+  if oauth_settings['vkontakte']
+    ENV['API_KEY']    ||= oauth_settings['vkontakte']['api_key']
+    ENV['API_SECRET'] ||= oauth_settings['vkontakte']['api_secret']
+  end
+
+  use OmniAuth::Builder do  
+    if ENV['API_KEY']
+      provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET'],
+        :scope => 'friends,audio,photos', :display => 'popup'
+    end
+  end
+end
