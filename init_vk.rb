d71ecc952cb7f77e7f2eda609e02c95701eaa08b
Ilya Nemihin <nemilya@gmail.com>
2012-05-25 16:17:36 +0600

    добавлена обработка настроек OmniAith для режима разработки и продуктового

--- init_vk.rb
+++ init_vk.rb
@@ -1,16 +1,21 @@
-config_path = File.join("config", "oauth.yml")
+configure :development do
+  config_path = File.join("config", "oauth.local.yml")
+
+  # в случае если тестируем локально, то убираем проверку сертификата
+  # иначе были сложности
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
+
+configure :production do
+  config_path = File.join("config", "oauth.yml")
+end
+
 if File.exists?(config_path)
   oauth_settings = YAML.load(File.read(config_path))
 
   if oauth_settings['vkontakte']
     ENV['API_KEY']    ||= oauth_settings['vkontakte']['api_key']
     ENV['API_SECRET'] ||= oauth_settings['vkontakte']['api_secret']
-  end
-
-  # в случае если тестируем локально, то убираем проверку сертификата
-  # иначе были сложности
-  configure :development do
-    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   end
 
   use OmniAuth::Builder do
